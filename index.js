061b2f3c151a84be6adda0ca2266534930d5bd61
ekball <90882418+ekball@users.noreply.github.com>
2021-12-01 20:35:08 -0500

    installed npm inquirer

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    title = 'What is the title of your project?',
+    description = 'Please describe your project.',
+    install = 'Please give the steps to install your project.',
+    usage = 'What is this project used to accomplish?',
+    contribution = 'What are the contribution guidelines?',
+    test = 'What are the test instructions for your project?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
